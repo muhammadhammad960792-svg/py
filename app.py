cf74b1e7f03ee2444a210e37ab8a2213e5656678
hammad <muhammadhammad960792@gmail.com>
2026-05-07 11:39:46 +0500

    operators and data types

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print("helleo world")
 print(9+2)
 print("subtraction of 8-2",8-2)
+#hello how areyou.
+import keyword
+print(keyword.kwlist)
+print("hi /n")
---
+++ s.py
@@ -0,0 +1,20 @@
+name="ali"
+weight=71.4
+num1=81
+she_is_cooking=False
+print("my name is ",name)
+print("data type is", type(name))
+print("value of num1",type(num1))
+print("data type of num1 is",type( she_is_cooking ))
+a=12
+b=20
+c=a+b
+d=b-a
+Mul=a*b
+div=b/a
+print(a)
+print(b)
+print(c)
+print(d)
+print(Mul)
+print(div)
