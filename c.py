8038de098c4ccfafaabc60f19fa435c3ed9dae6e
hammad <muhammadhammad960792@gmail.com>
2026-05-07 12:19:58 +0500

    Data Types And Different Operators.

---
+++ c.py
@@ -0,0 +1,25 @@
+name="Sudais"
+Weight=74.2
+applevalue=150
+He_is_writing=True
+print("name")
+print("my name is ", name)
+print("data type is",type(name))
+print("data type is", type(Weight))
+print("value of apple is",type(applevalue))
+print("data type is ",type(He_is_writing))
+#now for the operators
+a=8
+b=4
+c=a+b
+d=b-a
+mul=a*b
+div=b/a
+#now produce the output
+print(a)
+print(b)
+print(c)
+print(d)
+print(mul)
+print(div)
+#Thats all about the data typs and operators. 
