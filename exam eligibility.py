5ac440893ecabb081273caba3a1ed8dd4ec64b17
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-05-12 12:36:09 +0500

    Add files via upload

---
+++ exam eligibility.py
@@ -0,0 +1,16 @@
+marks=int(input("enter a marks"))
+if marks==70:
+    print("Then you are eligible for the exams or passed")
+elif marks==60:
+    print("Then you are eligible for the exams or passed with grace marks")
+elif marks==50:
+    print("Then you are eligible for the exams or passed with grace marks")
+elif marks==40:
+    print("Then you are eligible for the exams or passed with grace marks")
+elif marks==33:
+    print("Then you are eligible for the exams or passed with grace marks")
+else:
+    print("Then you are not eligible for the exams or Failed")
+     
+
+
