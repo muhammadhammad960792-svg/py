7323347d491a2848b865200b96f3bb94046e0e1e
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-05-21 11:40:39 +0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,30 @@
+def greet ():
+    print("Welcome to the street of Python")
+greet()
+
+def intro (name):
+    print("Hello", name, "Good morning!")
+intro("Jin Kazama")
+
+x=int(input("x "))
+y=int(input("y "))
+def multiply (x,y):
+    print(x*y)
+multiply(x,y)
+x= int(input("x "))
+y= int(input("y "))
+def divide (x,y):
+    print(x/y)
+divide(x,y)
+
+x= int(input("x"))
+y= int(input("y"))
+def add (x,y):
+    print(x+y)
+add(x,y)
+
+x=int(input("x ")) 
+y=int(input("y "))
+def subtract (x,y):
+    return x-y
+print(subtract(x,y))
