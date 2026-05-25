d9f6641ec81e6d9501b7ce9db98daf436d065970
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-05-25 12:07:00 +0500

    Add files via upload

---
+++ Hexagon shape.py
@@ -0,0 +1,9 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+for _ in range(6):
+    my_turtle.forward(100)
+    my_turtle.left(60)
+
+turtle.done()
---
+++ star shape.py
@@ -0,0 +1,9 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+for _ in range(5):
+    my_turtle.forward(150)
+    my_turtle.right(144)
+
+turtle.done()
