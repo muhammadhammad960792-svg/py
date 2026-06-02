a295aa847aa92f0b9184c1bc121f13a106e7fb8d
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-06-02 11:42:20 +0500

    Add files via upload

---
+++ oops .py
@@ -0,0 +1,19 @@
+class kakashi:
+    Name="Minato"
+    grade=10
+    def display(self):
+        print("my name is",self.Name)
+fing=kakashi()
+fing.display()
+
+
+class Animal:
+    Specie="Tiger"
+    age=1
+    def details(self):
+        print("I am ",self.Specie)
+    def myage(self):
+        print("my age is ",self.myage)
+Fox=Animal()
+Fox.details()
+Fox.myage()
