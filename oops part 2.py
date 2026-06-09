2d9b73e2e287c7e3d12c53542866185c63478562
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-06-09 11:53:14 +0500

    Add files via upload

--- oops part 2.py
+++ oops part 2.py
@@ -1,23 +1,27 @@
-class playlist:
-    def __init__(self,name,genre):
-        self.name=name
-        self.genere=self.genre
-        self.songs=[]
-        print(f"playlits",'{self.name}'((self.genre)) is ready!)
-              
-    def add_song(self,song):
+class Playlist:
+    def __init__(self, name, genre):
+        self.name = name
+        self.genre = genre
+        self.songs = []
+        print(f"Playlist '{self.name}' ({self.genre}) is ready!")
+
+    def add_song(self, song):
         self.songs.append(song)
-        print(f" '{song}' added to {self.name}.")
-        
-    def remove_song(remove,song):
-        if song if in self.songs:
-            self.songs.remove(song):
-            print(f"'{song}'removed.")
-        else
-            print(f" '{song}'not found in playlist>")
+        print(f"'{song}' added to {self.name}.")
+
+    def remove_song(self, song):
+        if song in self.songs:
+            self.songs.remove(song)
+            print(f"'{song}' removed.")
+        else:
+            print(f"'{song}' not found in playlist.")
+
     def display(self):
-        print(f"/n---{self.name} ({self.genre})---")
+        print(f"\n--- {self.name} ({self.genre}) ---")
+
         if self.songs:
-            for i ,songs in enumarate (self.songs,1):
-                print f" {i}. {songs}:
-        else
+            for i, song in enumerate(self.songs, 1):
+                print(f"{i}. {song}")
+        else:
+            print("No songs in the playlist.")
+
