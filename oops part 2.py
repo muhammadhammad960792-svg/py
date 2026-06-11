deb8dee2bd761091bd6947ca4db7c9b0e0077aef
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-06-11 11:33:42 +0500

    Add files via upload

--- oops part 2.py
+++ oops part 2.py
@@ -25,3 +25,22 @@ class Playlist:
         else:
             print("No songs in the playlist.")
 
+    def __del__ (self):
+     print(f"playlist  '{self.name}' has been deleted .Goodbye!")
+my__playlists=Playlist("Road Trip Mix","pop")
+while True:
+    print("\n1.add song 2.remove song 3.View playlist 4.Delete and Quit")
+    choice=input("Enter your choice:")
+    if choice =="1":
+        song=input("Enter your song name:")
+        my__playlists.add_song(song)
+    elif choice=="2":
+        song=input("Enter song to remove")
+        my__playlists.remove_song(song)
+    elif choice =="3":
+        my__playlists.display()
+    elif choice =="4":
+        del my__playlists
+        break
+    else:
+        print("Invalid Choice.Enter 1,2,3,or 4.")
