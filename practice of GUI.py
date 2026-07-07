a6ae802f932cdde0cf9c2e8aefaf65e4ea554b7a
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-07-07 11:42:37 +0500

    Add files via upload

---
+++ practice of GUI.py
@@ -0,0 +1,14 @@
+from tkinter import*
+window=Tk()
+window.title("tkinter library")
+window.geometry("500x500")
+lbl=Label(text="Welcome to the the tkinter",bg="gray",fg="black",width=50,height=2)
+lbl.pack()
+
+
+
+
+
+
+
+window.mainloop()
