e6664ac2e098a1829663bb921f09a87f3e8d02ac
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-07-30 11:42:49 +0500

    Add files via upload

---
+++ Asymptotic notation.py
@@ -0,0 +1,35 @@
+names =["vegeta", "goku", "gohan", "beerus", "whis"]
+scores = [90,  75,   88,    62,   95]
+n=len(scores)
+print("==== Score Tracker (n=",n, "players) ====")
+for i in range (n):
+    print(i + 1 , ".", names[i], " : ", scores[i],sep="")
+print()
+steps = 1
+print("Score at index 0 :", scores[0], "| steps =", steps, "| Theta(1) -tight bound")
+print()
+target = "gohan"
+steps = 0
+for name in names:
+    steps += 1
+    if name == target:
+        break
+print("Search for",target, "| steps=", steps," |Omega(1) - best case lower bound")
+
+target = "whis"
+steps = 0
+for name in names:
+    steps += 1
+    if name == target:
+        break
+print("Search for",target, "| stpes=", steps," | O(n) =",n,"- worst case upper bound")
+
+steps = 0
+target_sum = 0
+print("pairs with total score =",target_sum, ":")
+for i in range(n):
+    for j in range (i + 1, n):
+        steps += 1
+        if scores[i] + scores[j] == target_sum:
+            print(" ", names[i], "+", names[j], "=", scores[i] + scores[j])
+            
