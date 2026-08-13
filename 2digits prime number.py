a0b20c1bcd6c82a35962d7e704676544bf2ad7cd
muhammadhammad960792-svg <muhammadhammad960792@gmail.com>
2026-08-13 11:52:55 +0500

    Add files via upload

---
+++ 2digits prime number.py
@@ -0,0 +1,13 @@
+print("Two-Digit Prime Numbers (11 to 99)")
+print("----------------------------------")
+
+for num in range(11, 100):
+    is_prime = True
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num)
